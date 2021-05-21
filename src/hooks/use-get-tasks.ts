759a70c95eb09a286ced4a38c3abdec3f0cd0756
K-shir0 <kakishiro2190394@gmail.com>
2021-05-22 03:34:11 +0900

    fix: hooksを削除

--- src/hooks/use-get-tasks.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from '../domain/models/task';
-
-type ReturnValue = {
-  tasks: Task[];
-};
-
-// const useGetTasks = (): ReturnValue => {
-//   const tasks = useSelector<TaskState, Task[]>(state => state.tasks);
-// };
